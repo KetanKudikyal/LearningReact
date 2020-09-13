f9d61eb1ce8f8abbff55cc52987b2598e5da8189
Ketan kudikyal <66976099+KetanKudikyal@users.noreply.github.com>
2020-09-13 19:10:26 +0530

    changed button text to Subscribe

--- src/Button.js
+++ src/Button.js
@@ -12,7 +12,7 @@ import './Button.css';
 
 const Button = () => {
  return       <div className='tc'>
-             <a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-gray" href="https://www.youtube.com/channel/UCscCgZoyQl7jpuCxD1vNdrw?view_as=subscriber">Button Text</a>
+             <a class="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-gray" href="https://www.youtube.com/channel/UCscCgZoyQl7jpuCxD1vNdrw?view_as=subscriber">Subscribe</a>
             {/*<button className="stylii">Subscribe</button>*/}
             
             {/**<button className="stylii">Subscribe</button>
